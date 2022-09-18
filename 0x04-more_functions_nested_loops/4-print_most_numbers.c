0eb32dceb9fee99aa4ad7d7b83600d79fb639de8
timothy adigun <adigun.timothy@gmail.com>
2022-09-18 06:38:48 +0100

    modified 4-print_most_numbers file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,11 +12,10 @@ void print_most_numbers(void)
 
 	while (i < 58)
 	{
-		if (i == 2 || i == 4)
+		if (i != 50 && i != 52)
 		{
-			continue;
+			_putchar(i);
 		}
-		_putchar(i);
 		i++;
 	}
 	_putchar('\n');
