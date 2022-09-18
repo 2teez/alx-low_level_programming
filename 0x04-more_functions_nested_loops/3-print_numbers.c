7c157f4416efe4de5d081ac3af362e27e4896ac2
timothy adigun <adigun.timothy@gmail.com>
2022-09-18 05:41:59 +0100

    added 3-print_numbers file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9, then returns void
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
