0c1183171302d2517bcbe5b4a8b285deec442808
timothy adigun <adigun.timothy@gmail.com>
2022-09-18 05:29:20 +0100

    added mul file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - returns a product of two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: product of two integer, multiplied
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
