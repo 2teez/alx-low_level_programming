79fb17badac31890db77f24c815196f2bb63a17d
timothy adigun <adigun.timothy@gmail.com>
2022-09-20 17:07:22 +0100

    7-puts_half added

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * puts_half - prints squares
+ * @str: the first parameter
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+
+	int length_of_the_string;
+
+	int i;
+
+	int n;
+
+	char *c;
+
+	length_of_the_string = 0;
+
+	c = str;
+
+	while (*str++)
+	{
+		length_of_the_string++;
+	}
+
+	if (length_of_the_string % 2 != 0)
+	{
+		n = (length_of_the_string - 1) / 2;
+	}
+	else
+	{
+		n = length_of_the_string / 2;
+	}
+
+	for (i = n; i < length_of_the_string; i++)
+	{
+		_putchar(c[i]);
+	}
+
+	_putchar('\n');
+}
