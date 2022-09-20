1cba40934f623e18b275a9415779f59acd42fe7e
timothy adigun <adigun.timothy@gmail.com>
2022-09-20 15:58:10 +0100

    5-rev_string.c modified

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,27 @@
 #include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strncpy - prints squares
+ * @s: the first parameter
+ * @c: the second parameter
+ * @len: the third parameter
+ * Return: void
+ */
+
+void _strncpy(char *s, char *c, int len)
+{
+
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		s[i] = c[i];
+	}
+
+	s[len] = '\0';
+}
+
 /**
  * rev_string - prints squares
  * @s: the first parameter
@@ -8,20 +31,23 @@
 void rev_string(char *s)
 {
 	int len;
-
 	int i;
-
+	int j;
 	char *c;
 
 	len = _strlen(s);
-
-	c = s;
-
-	for (i = len - 1; i >= 0; i--)
+	j = 0;
+	c = (char *)malloc(len * sizeof(char) + 1);
+	if (c != NULL)
 	{
-		_putchar(c[i]);
+		for (i = len - 1; i >= 0; i--)
+		{
+			*(c + j) = *(s + i);
+			j++;
+		}
 	}
-	*s = c[i];
+	c[j] = '\0';
+	_strncpy(s, c, len);
 }
 
 /**
