3c7ebdc1c15411fd788e33a257bbbc620b0f6017
timothy adigun <adigun.timothy@gmail.com>
2022-09-20 06:51:54 +0100

    added 4-print-rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - prints squares
+ * @s: the first parameter
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+
+	char *c;
+
+	c = s;
+
+	while (*s++)
+		;
+	while (s--)
+	{
+		_putchar(*s);
+		if (c == s)
+		{
+			break;
+		}
+	}
+	_putchar('\n');
+}
