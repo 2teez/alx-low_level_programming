3544832632047f6ef2388df421ce37f75616edf9
timothy adigun <adigun.timothy@gmail.com>
2022-09-25 09:49:24 +0100

    modified strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -55,6 +55,8 @@ int _strcmp(char *s1, char *s2)
 
 	int size;
 
+	int rvalue;
+
 	size = 0;
 
 	if (count_it(s1) >= count_it(s2))
@@ -71,14 +73,15 @@ int _strcmp(char *s1, char *s2)
 
 	if (s1 > s2)
 	{
-		return (-15);
+		rvalue = -15;
 	}
 	else if (s2 > s1)
 	{
-		return (15);
+		rvalue = 15;
 	}
 	else
 	{
-		return (0);
+		rvalue = 0;
 	}
+	return (rvalue);
 }
