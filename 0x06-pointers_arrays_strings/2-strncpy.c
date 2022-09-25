87b2c62b49eaef109047b0b5a71011bba7681d07
timothy adigun <adigun.timothy@gmail.com>
2022-09-25 04:49:47 +0100

    modified files

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,53 +1,24 @@
 #include "main.h"
 
 /**
- * count_it - count strings
- * @str: the first parameter
- * Return: int
- */
-int count_it(char *str)
-{
-	int count;
-
-	count = 0;
-
-	while (*str != '\0')
-	{
-		count++;
-		str++;
-	}
-
-	return (count);
-}
-
-/**
- * _strncat - combine two string
+ * _strncpy - combine two string
  * @dest: the first parameter
  * @src: the second parameter
  * @n: the third parameter
  * Return: char*
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	char *tmp;
-
-	int dest_ct;
-
-	int counter;
 
 	int i;
 
 	tmp = src;
 
-	dest_ct = count_it(dest);
-
-	counter = 0;
-
-	for (i = dest_ct; i < (dest_ct + n); i++)
+	for (i = 0; i < n; i++)
 	{
-		dest[i] = tmp[counter];
-		counter++;
+		dest[i] = tmp[i];
 	}
 
 	return (dest);
