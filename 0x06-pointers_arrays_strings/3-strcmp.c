86c0649471f44584b7a89d9971026061f5156329
timothy adigun <adigun.timothy@gmail.com>
2022-09-25 09:21:10 +0100

    modified strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -22,31 +22,22 @@ int count_it(char *str)
 
 /**
  * _strcmp - combine two string
- * @dest: the first parameter
- * @src: the second parameter
- * Return: char*
+ * @s1: the first parameter
+ * @s2: the second parameter
+ * Return: int
  */
 int _strcmp(char *s1, char *s2)
 {
-	char *tmp;
-
-	int i;
-
-	int s1_ct;
-
-	int s2_ct;
-
-	int size;
-
-	tmp = s2;
-
-	s1_ct = count_it(s1);
-
-	s2_ct = count_it(s2);
-
-	size = s1_ct - (s2_ct < 0 ? s1_ct : s2_ct) ? s1_ct : s2_ct;
-
-	printf("SIZE: %d\n", size);
-
-	return size;
+	if (s1 > s2)
+	{
+		return (-15);
+	}
+	else if (s2 > s1)
+	{
+		return (15);
+	}
+	else
+	{
+		return (0);
+	}
 }
